d29f05841388b762314701bd17831cb02224d9ab
Bryan <bbjt11@gmail.com>
2026-03-25 00:15:53 -0500

    set up simple program.cs and controller along with launch settings and nuget packages to start using a sample endpoint for front end development.
    WIP to get https working on the .net 10 solution.

---
+++ .NET_10/Solution1/API/Controllers/SimpleController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SimpleController
+    {
+        private readonly ILogger<SimpleController> _logger;
+        public SimpleController(ILogger<SimpleController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("hello")]
+        public string HelloWorld()
+        {
+            _logger.LogInformation("HelloWorld endpoint was called.");
+            return "Hello, World!";
+        }
+    }
+}
--- .NET_10/Solution1/API/Program.cs
+++ .NET_10/Solution1/API/Program.cs
@@ -1,1 +1,55 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi;
+using System.IO;
+
+var builder = WebApplication.CreateBuilder(args);
+
+Console.WriteLine($"ContentRootPath: {builder.Environment.ContentRootPath}");
+Console.WriteLine($"CurrentDirectory: {Directory.GetCurrentDirectory()}");
+//Console.WriteLine($"appsettings.json exists: {File.Exists(Path.Combine(builder.Environment.ContentRootPath, \"appsettings.json\"))}")
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "ToDo API",
+        Description = "An ASP.NET Core Web API for managing ToDo items",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+});
+
+var app = builder.Build();
+
+var environment = app.Configuration.GetValue<string>("Environment");
+
+if (environment == "Development")
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+//app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
